76246cc90180ac9c2f1054d6c3718ee27614ca78
lukasz-migas <lukas.migas@yahoo.com>
2023-08-13 12:13:23 +0100

    Update typing.py

--- src/koyo/typing.py
+++ src/koyo/typing.py
@@ -2,13 +2,14 @@
 import typing as ty
 from pathlib import Path
 
-import numpy as np
-from scipy import sparse
+if ty.TYPE_CHECKING:
+    import numpy as np
+    from scipy import sparse
 
 __all__ = ("ArrayLike", "PathLike", "SparseArray", "CompressedSparseArray")
 
-ArrayLike = ty.Union[ty.List, ty.Tuple, np.ndarray, sparse.csc_matrix, sparse.csr_matrix]
-SimpleArrayLike = ty.TypeVar("SimpleArrayLike", ty.List, np.ndarray, ty.Iterable)
-CompressedSparseArray = ty.Union[sparse.csc_matrix, sparse.csr_matrix]
-SparseArray = ty.Union[sparse.csc_matrix, sparse.csr_matrix, sparse.coo_matrix]
+ArrayLike = ty.Union[ty.List, ty.Tuple, "np.ndarray", "sparse.csc_matrix", "sparse.csr_matrix"]
+SimpleArrayLike = ty.TypeVar("SimpleArrayLike", ty.List, "np.ndarray", ty.Iterable)
+CompressedSparseArray = ty.Union["sparse.csc_matrix", "sparse.csr_matrix"]
+SparseArray = ty.Union["sparse.csc_matrix", "sparse.csr_matrix", "sparse.coo_matrix"]
 PathLike = ty.Union[str, Path]
