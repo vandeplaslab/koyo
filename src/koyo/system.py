10fc7b5244615593a5ed0389cd79e8d238d744bc
lukasz-migas <lukas.migas@yahoo.com>
2023-12-13 10:32:49 +0000

    Update system.py

--- src/koyo/system.py
+++ src/koyo/system.py
@@ -1,9 +1,15 @@
 """System utilities."""
-import sys
+import os
 import platform
+import sys
 
 IS_WIN = sys.platform == "win32"
 IS_LINUX = sys.platform == "linux"
 IS_MAC = sys.platform == "darwin"
 IS_MAC_ARM = IS_MAC and platform.processor() == "arm"
 IS_PYINSTALLER = getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS")
+
+
+def is_envvar(key: str, value: str) -> bool:
+    """Check if an environment variable is set."""
+    return key in os.environ and os.environ[key] == str(value)
