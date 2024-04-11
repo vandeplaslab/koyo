f956dd793d1c365328a47fff82f3d904112a95f7
lukasz-migas <lukas.migas@yahoo.com>
2024-04-11 14:45:48 +0100

    Create test_click.py

---
+++ tests/test_click.py
@@ -0,0 +1,31 @@
+"""Test that click produces the expected output."""
+import pytest
+from koyo.click import parse_arg
+
+
+@pytest.mark.parametrize(
+    "arg, expected",
+    [
+        ("n=5", ("n", 5)),
+        ("n=5.0", ("n", 5.0)),
+        ("n=[10,20,30]", ("n", [10, 20, 30])),
+    ],
+)
+def test_parse_arg(arg, expected):
+    res_key, res_value = parse_arg(arg, "")
+    assert res_key == expected[0], f"Expected {expected[0]}, got {res_key}"
+    assert res_value == expected[1], f"Expected {expected[1]}, got {res_value}"
+
+
+@pytest.mark.parametrize(
+    "arg, expected",
+    [
+        ("--un:n=5", ("n", 5)),
+        ("--un:n=5.0", ("n", 5.0)),
+        ("--un:n=[10,20,30]", ("n", [10, 20, 30])),
+    ],
+)
+def test_parse_arg(arg, expected):
+    res_key, res_value = parse_arg(arg, "--un:")
+    assert res_key == expected[0], f"Expected {expected[0]}, got {res_key}"
+    assert res_value == expected[1], f"Expected {expected[1]}, got {res_value}"
