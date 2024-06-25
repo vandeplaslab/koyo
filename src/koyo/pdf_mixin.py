5f48cd56c66692e0bcc02366e4c5288a038cbab5
lukasz-migas <lukas.migas@yahoo.com>
2024-06-25 15:12:32 +0100

    Update pdf_mixin.py

--- src/koyo/pdf_mixin.py
+++ src/koyo/pdf_mixin.py
@@ -1,8 +1,16 @@
 """PDF export mixin class."""
+from __future__ import annotations
+
+import typing as ty
 from contextlib import contextmanager
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+import numpy as np
 from loguru import logger
+
+if ty.TYPE_CHECKING:
+    from matplotlib.backends.backend_pdf import PdfPages
 
 
 class PDFMixin:
@@ -24,7 +32,7 @@ class PDFMixin:
         return Path(self.filename).with_suffix(".pdf")
 
     @property
-    def pdf(self):
+    def pdf(self) -> PdfPages | None:
         """Return PDF figure."""
         if self._pdf is None and self.as_pdf:
             from matplotlib.backends.backend_pdf import PdfPages
@@ -33,25 +41,25 @@ class PDFMixin:
         return self._pdf
 
     @pdf.setter
-    def pdf(self, value):
+    def pdf(self, value: PdfPages | None) -> None:
         """Set PDF figure."""
         if self._pdf is not None:
             self._pdf.close()
         self._pdf = value
 
     @contextmanager
-    def _export_figures(self):
+    def _export_figures(self) -> ty.Generator[None, None, None]:
         """Export figures."""
         import matplotlib
 
         matplotlib.use("agg")
 
         yield
-        if self.as_pdf:
-            self.pdf.close()
+        if self.as_pdf and hasattr(self.pdf, "close"):
+            self.pdf.close()  # type: ignore[union-attr]
             self._pdf = None
 
-    def _export_title(self, title: str):
+    def _export_title(self, title: str) -> None:
         from koyo.pdf import export_title
 
         export_title(self.pdf, title)
@@ -68,14 +76,14 @@ class PDFMixin:
     def _export_figure(
         self,
         filename: Path,
-        fig,
-        face_color=None,
-        bbox_inches="tight",
-        dpi=150,
+        fig: plt.Figure,
+        face_color: str | np.ndarray | None = None,
+        bbox_inches: str = "tight",
+        dpi: int = 150,
         override: bool = False,
         if_empty: str = "warn",
-        **kwargs,
-    ):
+        **kwargs: ty.Any,
+    ) -> None:
         """Export figure to file."""
         from koyo.pdf import export_figure
 
@@ -86,7 +94,7 @@ class PDFMixin:
         export_figure(self.pdf, filename, fig, face_color, bbox_inches, dpi, override, **kwargs)
 
     @staticmethod
-    def _inform_on_empty(if_empty: str = "warn"):
+    def _inform_on_empty(if_empty: str = "warn") -> None:
         """Inform the user if the figure was empty."""
         if if_empty == "none":
             return
